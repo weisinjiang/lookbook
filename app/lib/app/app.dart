100c3d347678bcafb6603ebea1ba349287b02ba3
weisinjiang <54108149+weisinjiang@users.noreply.github.com>
2022-02-01 08:49:02 -0500

    Added firebase project and hid secrets

---
+++ app/lib/app/app.dart
@@ -0,0 +1,12 @@
+import 'package:app/widgets/look_canvas.dart';
+import 'package:flutter/material.dart';
+
+class StartApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(  
+      home: LookCanvas(),
+    );
+  }
+  
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,24 +1,17 @@
+import 'package:app/app/app.dart';
+import 'package:app/firebase_options.dart';
 import 'package:app/widgets/look_canvas.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(StartApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Lookbook',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LookCanvas(),
-    );
-  }
-}
 
 
