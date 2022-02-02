dfae47e27ebb9026ca83d10556df90a296e33919
weisinjiang <54108149+weisinjiang@users.noreply.github.com>
2022-02-02 12:37:20 -0500

    added web_layout_view

--- app/lib/core/models/device_details.dart
+++
@@ -1,7 +0,0 @@
-class DeviceDetails {
-  bool isMobile = false;
-  bool isTablet = false;
-  bool isLaptop = false;
-  bool isDesktop = false;
-  bool isWideDesktop = false;
-}
--- app/lib/core/utils/web_layout_view.dart
+++ app/lib/core/utils/web_layout_view.dart
@@ -1,6 +1,13 @@
-
-import 'package:app/core/models/device_details.dart';
 import 'package:flutter/material.dart';
+
+class DeviceDetails {
+  bool isMobile = false;
+  bool isTablet = false;
+  bool isComputer = false;
+  bool is1080 = false;
+  bool is1440 = false;
+  bool is4k = false;
+}
 
 class WebLayoutView extends StatelessWidget {
 
@@ -12,10 +19,48 @@ class WebLayoutView extends StatelessWidget {
   
   }) : super(key: key);
 
+  DeviceDetails getDeviceDetails(BoxConstraints constraints) {
+    DeviceDetails deviceDetails = DeviceDetails();
+    double width = constraints.maxWidth;
+    if (width >= 2160) {
+      deviceDetails.isComputer = true;
+      deviceDetails.is4k = true;
+    }
+    else if (width >= 1440) {
+      deviceDetails.is1440 = true;
+      deviceDetails.isComputer = true;
+    }
+    else if (width >= 1080) {
+      deviceDetails.is1080 = true;
+      deviceDetails.isComputer = true;
+    }
+    else if (width >= 992) {
+      deviceDetails.isComputer = true;
+    }
+    else if (width >= 768) {
+      deviceDetails.isTablet = true;
+      deviceDetails.isMobile = true;
+    }
+    else {
+      deviceDetails.isMobile = true;
+    }
+    return deviceDetails;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: SizedBox(  
+            height: constraints.maxHeight,
+            child: SingleChildScrollView(  
+              child: builder(context, constraints, getDeviceDetails(constraints)),
+            ),
+          ),
+        );
+      }
+    );
   }
   
 }
